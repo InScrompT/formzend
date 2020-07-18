f1578b3019a458de8292d4aca026aba6e20501f3
xXAlphaManXx <xalphamanx@tuta.io>
2020-07-18 20:57:51 +0530

    Use local bulma instead off CDN

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>@yield('title') - FormZend</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         @yield('content')
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
