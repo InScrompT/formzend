418509dcb3ed4b690851e6155879f7a22fab1485
xXAlphaManXx <xalphamanx@tuta.io>
2020-07-20 12:06:07 +0530

    Add events

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,10 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        'App\Events\NewAccount' => [
+            'App\Listeners\SaveNewAccount',
+            'App\Listeners\SendVerification',
+        ],
     ];
 
     /**
