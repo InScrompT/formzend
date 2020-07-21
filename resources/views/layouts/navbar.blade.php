7e74c932bf58aea66fed3b670bc4221e1de59d95
xXAlphaManXx <xalphamanx@tuta.io>
2020-07-22 00:42:10 +0530

    Make navbar mobile friendly

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,13 +4,20 @@
             <div class="navbar-item">
                 <a class="is-size-4 has-text-primary" href="{{ route('home') }}">FormZend</a>
             </div>
+            <a role="button" class="navbar-burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
         </div>
-        <div class="navbar-end">
-            <div class="navbar-item">
-                <a href="#" class="has-text-primary">Pricing</a>
-            </div>
-            <div class="navbar-item">
-                <a href="#" class="has-text-primary">FAQ</a>
+        <div class="navbar-menu" id="navMenu">
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <a href="#" class="has-text-primary">Pricing</a>
+                </div>
+                <div class="navbar-item">
+                    <a href="#" class="has-text-primary">FAQ</a>
+                </div>
             </div>
         </div>
     </div>
