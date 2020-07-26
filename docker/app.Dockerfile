07c34130ba0f0d42be8d828cc9eb4098682c878d
xXAlphaManXx <xalphamanx@tuta.io>
2020-07-27 01:34:53 +0530

    Remove queue from the docker stack

--- docker/app.Dockerfile
+++ docker/app.Dockerfile
@@ -13,5 +13,7 @@ RUN chown -R www-data:www-data /var/www \
     && chmod u+x /usr/local/bin/start
 RUN apk add --no-cache libxml2-dev \
     && docker-php-ext-install mysqli pdo pdo_mysql xml
+RUN composer install --optimize-autoloader --no-dev \
+    && php artisan route:cache && php artisan view:cache
 
 CMD ["/usr/local/bin/start"]
--- docker/start.sh
+++ docker/start.sh
@@ -2,19 +2,5 @@
 
 set -e
 
-role=${CONTAINER_ROLE:-app}
-env=${APP_ENV:-production}
-
-if [ "$env" != "local" ]; then
-    echo "Caching configuration..."
-    (composer install --optimize-autoloader --no-dev && php artisan config:cache && php artisan route:cache && php artisan view:cache)
-fi
-
-if [ "$role" = "app" ]; then
-    exec php-fpm
-elif [ "$role" = "queue" ]; then
-    php artisan queue:work --verbose --tries=3 --timeout=90
-else
-    echo "Could not match the container role \"$role\""
-    exit 1
-fi
+php artisan queue:work --verbose --tries=3 --timeout=90 &
+exec php-fpm
