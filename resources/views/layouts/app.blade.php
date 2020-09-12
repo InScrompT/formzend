755cd52afea60138e1868957619c9b0802325f1a
Karan Sanjeev <xalphamanx@tuta.io>
2020-09-12 22:28:00 +0530

    Add simpleanalytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        @yield('head')
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title') - FormZend</title>
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    </head>
-    <body>
-        @yield('content')
-        <script src="{{ asset('js/app.js') }}"></script>
-    </body>
+    @yield('head')
+
+    <title>@yield('title') - FormZend</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+    @yield('content')
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script async defer src="https://funny.formzend.com/latest.js"></script>
+    <noscript><img src="https://funny.formzend.com/noscript.gif" alt="" /></noscript>
+</body>
+
 </html>
