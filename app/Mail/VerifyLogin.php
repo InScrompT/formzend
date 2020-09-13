468a0352a03e87695567d6a44ab3058a3c464fde
Karan Sanjeev <xalphamanx@tuta.io>
2020-09-13 14:55:06 +0530

    Remove queue in verifylogin page

--- app/Mail/VerifyLogin.php
+++ app/Mail/VerifyLogin.php
@@ -3,11 +3,10 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class VerifyLogin extends Mailable implements ShouldQueue
+class VerifyLogin extends Mailable
 {
     use Queueable, SerializesModels;
 
