34d55365168357e271ee64ae2023ccb7c0bc6693
Karan Sanjeev <xalphamanx@tuta.io>
2020-09-13 17:17:48 +0530

    Make navigation less clutered

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -12,12 +12,6 @@
         </div>
         <div class="navbar-menu" id="navMenu">
             <div class="navbar-end">
-                <div class="navbar-item">
-                    <a href="{{ route('home') }}#pricing" class="has-text-primary">Pricing</a>
-                </div>
-                <div class="navbar-item">
-                    <a href="{{ route('home') }}#faq" class="has-text-primary">FAQ</a>
-                </div>
                 @user
                     <div class="navbar-item">
                         <a href="{{ route('dashboard') }}" class="has-text-primary">Dashboard</a>
@@ -26,6 +20,12 @@
                         <a href="{{ route('logout') }}" class="has-text-primary">Logout</a>
                     </div>
                 @else
+                    <div class="navbar-item">
+                        <a href="{{ route('home') }}#pricing" class="has-text-primary">Pricing</a>
+                    </div>
+                    <div class="navbar-item">
+                        <a href="{{ route('home') }}#faq" class="has-text-primary">FAQ</a>
+                    </div>
                     <div class="navbar-item">
                         <a href="{{ route('login') }}" class="has-text-primary">Login</a>
                     </div>
