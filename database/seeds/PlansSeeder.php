7d1e67dfc2885ef50538b74993b57d19d618d74f
Karan Sanjeev <xalphamanx@tuta.io>
2020-09-15 20:47:17 +0530

    Write seeders

---
+++ database/seeds/PlansSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Plan;
+use Illuminate\Database\Seeder;
+
+class PlansSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->createFreePlan();
+        $this->createBuddingPlan();
+    }
+
+    public function createFreePlan()
+    {
+        $plan = new Plan();
+
+        $plan->amount = 0;
+        $plan->name = 'Free';
+        $plan->quantity = 150;
+        $plan->available = false;
+
+        $plan->save();
+    }
+
+    public function createBuddingPlan()
+    {
+        $plan = new Plan();
+
+        $plan->amount = 2;
+        $plan->quantity = 2000;
+        $plan->name = 'Budding';
+        $plan->available = true;
+
+        $plan->save();
+    }
+}
