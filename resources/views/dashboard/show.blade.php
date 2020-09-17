e1c4f9b2aebe716c9c4dc61ab9189f1ebabca22b
Karan Sanjeev <xalphamanx@tuta.io>
2020-09-17 16:36:54 +0530

    Remove website count in dashboard

--- resources/views/dashboard/show.blade.php
+++ resources/views/dashboard/show.blade.php
@@ -26,37 +26,29 @@
                         <p class="is-size-4">Welcome back {{ $user->email }}</p>
                     </div>
 
-                    @if ($user->websites->count())
-                        <div class="panel">
-                            <div class="panel-heading">
-                                Your websites
-                            </div>
-                            <div class="panel-tabs">
-                                <a href="#" class="is-active">Verified</a>
-                                <a onclick="stillDev()" class="has-text-primary">Unverified</a>
-                                <a href="#" onclick="stillDev()" class="has-text-primary">Deactivated</a>
-                            </div>
+                    <div class="panel">
+                        <div class="panel-heading">
+                            Your websites
+                        </div>
+                        <div class="panel-tabs">
+                            <a href="#" class="is-active">Verified</a>
+                            <a onclick="stillDev()" class="has-text-primary">Unverified</a>
+                            <a href="#" onclick="stillDev()" class="has-text-primary">Deactivated</a>
+                        </div>
 
-                            @foreach ($user->websites as $website)
-                                <a href="{{ route('dashboard.website.submissions', [
-                                    $user->id,
-                                    $website->id
-                                ]) }}" class="panel-block">
-                                    <span class="tags has-addons mr-4">
-                                        <div class="tag is-success is-light">Submissions</div>
-                                        <div class="tag">{{ $website->submissions->count() }}</div>
-                                    </span>
-                                    {{ $website->url }}
-                                </a>
-                            @endforeach
-                        </div>
-                    @else
-                        <div class="card has-background-primary">
-                            <div class="card-content">
-                                <p class="is-size-5 has-text-white">You have no websites</p>
-                            </div>
-                        </div>
-                    @endif
+                        @foreach ($user->websites as $website)
+                            <a href="{{ route('dashboard.website.submissions', [
+                                $user->id,
+                                $website->id
+                            ]) }}" class="panel-block">
+                                <span class="tags has-addons mr-4">
+                                    <div class="tag is-success is-light">Submissions</div>
+                                    <div class="tag">{{ $website->submissions->count() }}</div>
+                                </span>
+                                {{ $website->url }}
+                            </a>
+                        @endforeach
+                    </div>
 
                     <div class="panel">
                         <div class="panel-heading">Summary</div>
