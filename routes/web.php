f6cf4a711221a48815cf42977400949d946b2e80
Karan Sanjeev <yo@alphaman.me>
2020-09-24 23:06:52 +0530

    Remove trailing slashes where not required.

--- routes/web.php
+++ routes/web.php
@@ -14,38 +14,38 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome')->name('home');
-Route::view('/privacy', 'privacy')->name('privacy');
-Route::view('/terms', 'terms')->name('terms');
+Route::view('privacy', 'privacy')->name('privacy');
+Route::view('terms', 'terms')->name('terms');
 
-Route::get('/auth/login', 'AuthController@showLogin')
+Route::get('auth/login', 'AuthController@showLogin')
     ->middleware('guest')
     ->name('login');
-Route::get('/auth/logout', 'AuthController@logout')
+Route::get('auth/logout', 'AuthController@logout')
     ->middleware('auth')
     ->name('logout');
-Route::get('/auth/login/account/{account:id}', 'AuthController@loginUser')
+Route::get('auth/login/account/{account:id}', 'AuthController@loginUser')
     ->middleware('signed', 'guest')
     ->name('login.verify');
 
-Route::get('/verify/{account}/website/{website:id}', 'WebsiteController@verify')
+Route::get('verify/{account}/website/{website:id}', 'WebsiteController@verify')
     ->middleware('signed')
     ->name('website.verify');
 
-Route::get('/dashboard', 'DashboardController@show')
+Route::get('dashboard', 'DashboardController@show')
     ->name('dashboard');
-Route::get('/dashboard/{account}/website/{website:id}/submissions', 'DashboardController@listSubmissions')
+Route::get('dashboard/{account}/website/{website:id}/submissions', 'DashboardController@listSubmissions')
     ->name('dashboard.website.submissions');
-Route::get('/dashboard/{account}/website/{website:id}/submissions/{submission:id}', 'DashboardController@showSubmission')
+Route::get('dashboard/{account}/website/{website:id}/submissions/{submission:id}', 'DashboardController@showSubmission')
     ->name('dashboard.website.submissions.show');
 
-Route::get('/plans', 'PaymentController@showPlans')->name('plans');
-Route::get('/plans/{plan}', 'PaymentController@buyPlan')->name('plans.buy');
+Route::get('plans', 'PaymentController@showPlans')->name('plans');
+Route::get('plans/{plan}', 'PaymentController@buyPlan')->name('plans.buy');
 Route::get('plans/payment/cancel', 'PaymentController@paymentCancelled')->name('plans.payment.cancelled');
 
 Route::post('plans/payment/done', 'PaymentController@paymentCallback')->name('plans.payment.done');
-Route::post('/auth/login', 'AuthController@processLogin')
+Route::post('auth/login', 'AuthController@processLogin')
     ->middleware('csrf', 'guest');
-Route::post('/verify/resend/{account}/website/{website:id}', 'WebsiteController@resendVerification')
+Route::post('verify/resend/{account}/website/{website:id}', 'WebsiteController@resendVerification')
     ->middleware('csrf')
     ->name('website.verify.resend');
 Route::post('/{email}', 'FormController@handleSubmission')
