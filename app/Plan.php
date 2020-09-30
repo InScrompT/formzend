7b70f6e34370f05ef291871538b97400caa58531
Karan Sanjeev <yo@alphaman.me>
2020-09-30 21:14:02 +0530

    Cast proper types in Plan Model

--- app/Plan.php
+++ app/Plan.php
@@ -32,6 +32,13 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Plan extends Model
 {
+    protected $casts = [
+        'amount' => 'int',
+        'quantity' => 'int',
+        'available' => 'boolean',
+        'is_subscription' => 'boolean',
+    ];
+
     public function accounts()
     {
         return $this->hasMany(Account::class);
