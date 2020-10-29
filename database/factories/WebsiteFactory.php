3bbf12a42711e03bc3ddf8db8556a38cb49f39c0
Karan Sanjeev <yo@alphaman.me>
2020-10-29 20:24:27 +0530

    Include Factories for testing

---
+++ database/factories/WebsiteFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Website;
+use App\Account;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class WebsiteFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Website::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'account_id' => Account::factory(),
+            'url' => $this->faker->domainName,
+            'verified' => true,
+        ];
+    }
+
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'verified' => false
+            ];
+        });
+    }
+}
