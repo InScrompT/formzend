4c3507218f22c2db270690831ca81964506e5e30
Karan Sanjeev <yo@alphaman.me>
2020-10-29 18:14:01 +0530

    Remove unused imports in Providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,6 @@
 
 namespace App\Providers;
 
-use App\Account;
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
