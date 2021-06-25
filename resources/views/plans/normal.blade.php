5a6bd2ec015740171d3dba7e9d58eff8a2205205
Karan Sanjeev <yo@alphaman.me>
2021-06-25 21:10:55 +0530

    Fix pricing page

--- resources/views/plans/normal.blade.php
+++ resources/views/plans/normal.blade.php
@@ -3,30 +3,28 @@
 
 <div class="columns is-centered">
     @foreach($normalPlan as $plan)
-        <div class="column is-4">
+        <div class="column is-half">
             <div class="card">
                 <div class="card-header">
                     <p class="card-header-title has-text-grey-dark">{{ $plan->name }}</p>
                 </div>
                 <div class="card-content">
-                    <span class="is-size-4">Get {{ $plan->quantity }} submissions</span>
-                    &mdash;
-                    <span class="is-size-5">
-                        For @if($plan->amount) just {{ $plan->amount }}$ @else Free @endif
-                    </span>
+                    <p class="is-size-4">Get {{ $plan->quantity }} submissions</p>
+                    <p class="is-size-5">
+                        &mdash; For @if($plan->amount) just {{ $plan->amount }}$ @else Free @endif
+                    </p>
 
-                    <br>
-                    <br>
-
-                    @if(strtolower($plan->name) === 'free')
-                        <a class="button is-fullwidth is-outlined" href="{{ route('dashboard') }}">
-                            Create free account
-                        </a>
-                    @else
-                        <a class="button is-fullwidth is-primary is-outlined" href="{{ route('plans.buy', $plan->id) }}">
-                            Buy {{ $plan->name }}
-                        </a>
-                    @endif
+                    <div class="mt-5">
+                        @if(strtolower($plan->name) === 'free')
+                            <a class="button is-fullwidth is-outlined" href="{{ route('dashboard') }}">
+                                Create free account
+                            </a>
+                        @else
+                            <a class="button is-fullwidth is-primary is-outlined" href="{{ route('plans.buy', $plan->id) }}">
+                                Buy {{ $plan->name }}
+                            </a>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
