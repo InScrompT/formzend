39dd9404f0f86bc22f1def466d218f12630246c5
Karan Sanjeev <yo@alphaman.me>
2022-01-22 15:14:13 +0530

    Delete all files

--- config/razorpay.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'api' => [
-        'key' => env('RAZORPAY_KEY'),
-        'secret' => env('RAZORPAY_SECRET')
-    ]
-];
--- resources/views/layouts/analytics.blade.php
+++
@@ -1,2 +0,0 @@
-<script async defer src="https://funny.formzend.com/latest.js"></script>
-<noscript><img src="https://funny.formzend.com/noscript.gif" alt="" /></noscript>
