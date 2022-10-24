4042e070ef9671f60da69da1834863c8c653563d
Karan Sanjeev <yo@alphaman.me>
2022-10-24 21:12:53 +0530

    Upgrade routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FormController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WebsiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,48 +25,65 @@ Route::view('terms', 'terms')->name('terms');
 Route::view('contact', 'contact')->name('contact');
 Route::view('refund', 'refund')->name('refund');
 
-Route::get('auth/login', 'AuthController@showLogin')
-    ->middleware('guest')
-    ->name('login');
-Route::get('auth/logout', 'AuthController@logout')
-    ->middleware('auth')
-    ->name('logout');
-Route::get('auth/login/account/{account:id}/key/{key}', 'AuthController@loginUser')
-    ->middleware('guest')
-    ->name('login.verify');
+Route::prefix('auth')
+    ->controller(AuthController::class)
+    ->group(function () {
+        Route::get('login', 'showLogin')
+            ->middleware('guest')
+            ->name('login');
+        Route::get('logout', 'logout')
+            ->middleware('auth')
+            ->name('logout');
+        Route::get('login/account/{account:id}/key/{key}', 'loginUser')
+            ->middleware('guest')
+            ->name('login.verify');
+    });
 
-Route::get('verify/account/{account:id}/website/{website:id}/remind', 'WebsiteController@remindVerification')
-    ->name('website.verify.remind');
-Route::get('verify/website/{link}/email/{email}', 'WebsiteVerificatiin@showVerificationNotice')
-    ->name('website.verify.show');
-Route::get('verify/website/{website:id}/key/{key}', 'WebsiteController@verify')
-    ->name('website.verify');
-Route::get('verify/resend/website/{website:id}', 'WebsiteController@resendVerification')
-    ->name('website.verify.resend');
+Route::prefix('verify')
+    ->controller(WebsiteController::class)
+    ->group(function () {
+        Route::get('account/{account:id}/website/{website:id}/remind', 'remindVerification')
+            ->name('website.verify.remind');
+        Route::get('website/{link}/email/{email}', 'showVerificationNotice')
+            ->name('website.verify.show');
+        Route::get('website/{website:id}/key/{key}', 'verify')
+            ->name('website.verify');
+        Route::get('resend/website/{website:id}', 'resendVerification')
+            ->name('website.verify.resend');
+    });
 
-Route::get('dashboard', 'DashboardController@show')
-    ->name('dashboard');
-Route::get('dashboard/unverified', 'DashboardController@unverified')
-    ->name('dashboard.websites.unverified');
-Route::get('dashboard/website/{website:id}/submissions', 'DashboardController@listSubmissions')
-    ->name('dashboard.website.submissions');
-Route::get('dashboard/submissions/{submission:id}', 'DashboardController@showSubmission')
-    ->name('dashboard.website.submissions.show');
+Route::prefix('dashboard')
+    ->controller(DashboardController::class)
+    ->group(function () {
+        Route::get('/', 'show')
+            ->name('dashboard');
+        Route::get('unverified', 'unverified')
+            ->name('dashboard.websites.unverified');
+        Route::get('website/{website:id}/submissions', 'listSubmissions')
+            ->name('dashboard.website.submissions');
+        Route::get('submissions/{submission:id}', 'showSubmission')
+            ->name('dashboard.website.submissions.show');
+    });
 
-Route::get('download/submissions/{submission:id}', 'FormController@downloadSubmission')
+Route::get('download/submissions/{submission:id}', [FormController::class, 'downloadSubmission'])
     ->name('download.submission');
-Route::get('download/website/{website:id}/submissions', 'DashboardController@exportSubmissions')
+Route::get('download/website/{website:id}/submissions', [DashboardController::class, 'exportSubmissions'])
     ->name('download.submissions');
 
-Route::get('plans', 'PaymentController@showPlans')->name('plans');
-Route::get('plans/{plan}', 'PaymentController@buyPlan')->name('plans.buy');
-Route::get('plans/payment/cancel', 'PaymentController@paymentCancelled')->name('plans.payment.cancelled');
+Route::prefix('plans')
+    ->controller(PaymentController::class)
+    ->group(function () {
+        Route::get('/', 'showPlans')->name('plans');
+        Route::get('{plan}', 'buyPlan')->name('plans.buy');
+        Route::get('payment/cancel', 'paymentCancelled')->name('plans.payment.cancelled');
 
-Route::get('profile', 'ProfileController@showProfile')->name('profile');
+        Route::post('payment/done', 'paymentCallback')->name('plans.payment.done');
+    });
 
-Route::post('plans/payment/done', 'PaymentController@paymentCallback')->name('plans.payment.done');
-Route::post('auth/login', 'AuthController@processLogin')
+Route::get('profile', [ProfileController::class, 'showProfile'])->name('profile');
+
+Route::post('auth/login', [AuthController::class, 'processLogin'])
     ->middleware('csrf', 'guest');
-Route::post('/{email}', 'FormController@handleSubmission')
+Route::post('/{email}', [FormController::class, 'handleSubmission'])
     ->middleware('check.email', 'check.email.verified')
     ->name('form');
